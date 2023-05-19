2b7ee643a28adcc13d6ca7ee328d283443a34fb3
hrhhh1997 <adarkwananakojonyan@yahoo.com>
2023-05-19 12:33:57 +0000

    Create and configure webpack env

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    mode: 'production',
     entry: path.resolve(__dirname, 'src/app.js'),
-    
-}
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/template.html',
+        })
+    ]
+};
