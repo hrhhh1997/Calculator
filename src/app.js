d70ca4e5d431893fa2ea7f971a98d031c4637e00
hrhhh1997 <adarkwananakojonyan@yahoo.com>
2023-05-22 10:12:28 +0000

    Create functionalities for calculator

--- src/app.js
+++ src/app.js
@@ -11,14 +11,73 @@ const operators = document.querySelectorAll('[data-operation]');
 const powerBtn = document.querySelector('[data-onOff]');
 const clearBtn = document.querySelector('[data-clear]');
 const clearAllBtn = document.querySelector('[data-clearEverything]');
+const decimalBtn = document.querySelector('[data-point');
 const equalBtn = document.querySelector('[data-equal]');
 const topScreen = document.querySelector('[data-chain-display]');
 const bottomScreen = document.querySelector('[data-current-display]');
 
-numbers.forEach(number => {
-    number.addEventListener('click', () => {
-        
-    })
-})
+powerBtn.addEventListener('click', powerOnOff);
+clearAllBtn.addEventListener('click', clearEverything);
+clearBtn.addEventListener('click', clearNumber);
+decimalBtn.addEventListener('click', appendDecimalIndicator);
+equalBtn.addEventListener('click', evaluate);
 
-function appendNumber()
+numbers.forEach(number => {
+    number.addEventListener('click', )
+});
+
+operators.forEach(operator => {
+    operator.addEventListener('click', );
+});
+
+function screenReset() {
+    bottomScreen.textContent = '';
+    resetScreen = false;
+}
+
+function appendNumber(number) {
+    if (bottomScreen.textContent === 0 || resetScreen) {
+        screenReset();
+    }
+    bottomScreen.textContent += number;
+}
+
+function appendDecimalIndicator() {
+    if (resetScreen) {
+        screenReset();
+    }
+    if (bottomScreen.textContent === '') {
+        bottomScreen.textContent = '0';
+    }
+    if (bottomScreen.textContent.includes('.')){
+        return bottomScreen.textContent += '.';
+    }
+}
+
+function clearEverything() {
+    bottomScreen.textContent = '0';
+    topScreen.textContent = '';
+    firstOperand = '';
+    secondOperand = '';
+    currentOperation = null;
+}
+
+function clearNumber() {
+    bottomScreen.textContent = bottomScreen.textContent.toString().slice(0, -1);
+}
+
+function setOperation(operator) {
+    if (currentOperation !== null) {
+        evaluate();
+    }
+    firstOperand = bottomScreen.textContent
+    currentOperation = operator;
+    topScreen.textContent = `${firstOperand} ${currentOperation}`;
+    resetScreen = true;
+}
+
+function evaluate() {
+    if (currentOperation === null || resetScreen) {
+        return;
+    }
+}
