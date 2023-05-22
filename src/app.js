7aeb1daab9e9c7e59ff5e51a8436c87d014ac478
hrhhh1997 <adarkwananakojonyan@yahoo.com>
2023-05-22 11:05:28 +0000

    Give complete functionality to calculator

--- src/app.js
+++ src/app.js
@@ -11,23 +11,24 @@ const operators = document.querySelectorAll('[data-operation]');
 const powerBtn = document.querySelector('[data-onOff]');
 const clearBtn = document.querySelector('[data-clear]');
 const clearAllBtn = document.querySelector('[data-clearEverything]');
-const decimalBtn = document.querySelector('[data-point');
+const decimalBtn = document.querySelector('[data-point]');
 const equalBtn = document.querySelector('[data-equal]');
 const topScreen = document.querySelector('[data-chain-display]');
 const bottomScreen = document.querySelector('[data-current-display]');
 
-powerBtn.addEventListener('click', powerOnOff);
+window.addEventListener('keydown', handleKeyboardInput);
+// powerBtn.addEventListener('click', powerOnOff);
 clearAllBtn.addEventListener('click', clearEverything);
 clearBtn.addEventListener('click', clearNumber);
 decimalBtn.addEventListener('click', appendDecimalIndicator);
 equalBtn.addEventListener('click', evaluate);
 
 numbers.forEach(number => {
-    number.addEventListener('click', )
+    number.addEventListener('click', () => appendNumber(number.textContent));
 });
 
 operators.forEach(operator => {
-    operator.addEventListener('click', );
+    operator.addEventListener('click', () => setOperation(operator.textContent));
 });
 
 function screenReset() {
@@ -36,22 +37,17 @@ function screenReset() {
 }
 
 function appendNumber(number) {
-    if (bottomScreen.textContent === 0 || resetScreen) {
+    if (bottomScreen.textContent === '0' || resetScreen)
         screenReset();
-    }
     bottomScreen.textContent += number;
 }
 
 function appendDecimalIndicator() {
-    if (resetScreen) {
-        screenReset();
-    }
-    if (bottomScreen.textContent === '') {
+    if (resetScreen) screenReset();
+    if (bottomScreen.textContent === '') 
         bottomScreen.textContent = '0';
-    }
-    if (bottomScreen.textContent.includes('.')){
+    if (bottomScreen.textContent.includes('.'))
         return bottomScreen.textContent += '.';
-    }
 }
 
 function clearEverything() {
@@ -67,9 +63,7 @@ function clearNumber() {
 }
 
 function setOperation(operator) {
-    if (currentOperation !== null) {
-        evaluate();
-    }
+    if (currentOperation !== null)  evaluate();
     firstOperand = bottomScreen.textContent
     currentOperation = operator;
     topScreen.textContent = `${firstOperand} ${currentOperation}`;
@@ -77,7 +71,45 @@ function setOperation(operator) {
 }
 
 function evaluate() {
-    if (currentOperation === null || resetScreen) {
+    if (currentOperation === null || resetScreen) return;
+    if (currentOperation === '÷' && bottomScreen.textContent === 0) {
+        alert('Undefined! Division by 0');
         return;
     }
+    secondOperand = bottomScreen.textContent;
+    bottomScreen.textContent = roundResult(calculate(currentOperation, firstOperand, secondOperand));
+    topScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+    currentOperation = null;
+}
+
+function roundResult(number) {
+    return Math.round(number * 1000) / 1000;
+}
+
+function handleKeyboardInput(e) {
+    if (e.key >= 0 && e.key <= 9) {
+        appendNumber(e.key);
+    };
+    if (e.key === '.') { 
+        appendDecimalIndicator();
+    };
+    if (e.key === '=' || e.key === 'Enter') { 
+        evaluate();
+    };
+    if (e.key === 'Backspace') {
+        clearNumber(); 
+    }
+    if (e.key === 'Escape') { 
+        clearEverything();
+    }
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
+        setOperation(convertOperator(e.key));
+    };
+      
+    function convertOperator(keyboardOperator) {
+        if (keyboardOperator === '/') return '÷'
+        if (keyboardOperator === '*') return 'x'
+        if (keyboardOperator === '-') return '-'
+        if (keyboardOperator === '+') return '+'
+      }
 }
